21470494bfd1e960a8de60e1c60dc7cc4cac8a02
Clare-Dean <cdean002@student.franciscan.edu>
2024-02-03 19:44:40 -0500

    finished_04

--- src/m3-functions_with_parameters.py
+++ src/m3-functions_with_parameters.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (4 pts)
+# DONE: 1. (4 pts)
 #
 #   As you saw in your pre-class quiz, we can also define functions that take
 #   information and do stuff with it. This information that the function uses
@@ -22,9 +22,13 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
-
+def name_and_color(name, color):
+    print(f"Your name is {name} and your favorite color is {color}!")
+name="John"
+color="green"
+name_and_color(name, color)
 ###############################################################################
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, let's do something similar, but let's use keyword arguments (this is
 #   also something that you saw in the pre-class materials).
@@ -54,3 +58,8 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+def display_user_info(name, email, age):
+    print(f"Name:{name}")
+    print(f"email:{email}")
+    print(f"age:{age}")
+display_user_info(name="John Smith", email= "jsmith@gmail.com", age= "19") 
